9583f5c9f8d79b4b8ef3d499d41f7342fc57df8e
miloszowi <miloszweb@gmail.com>
2022-12-01 18:53:53 +0100

    day 1 - ruby

---
+++ day01a_ruby/solution.rb
@@ -0,0 +1,12 @@
+elves = Array.new
+
+loop do
+	calories = 0
+	while (item_calories = (gets).to_i) and item_calories > 0
+		calories += item_calories
+	end
+	
+	break if calories == 0 else elves.append(calories)
+end
+
+print(elves.max)
